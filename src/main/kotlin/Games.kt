1c739c19c700a4e8038a4d1432c7851d8ae6d1d9
Nathalia Pinheiro <Thenaiapinn@gmail.com>
2025-09-13 19:40:39 -0300

    Adds exception handling and includes an option for adding a description.

--- src/main/kotlin/Games.kt
+++ src/main/kotlin/Games.kt
@@ -2,13 +2,11 @@ package org.example
 
 class Games(val title: String, val cover: String  ) {
 
-    val description = ""
+    var description = ""
     override fun toString(): String {
         return "My Games: \n" +
                 "Title: $title \n" +
                 "Cover: $cover \n" +
                 "Description: $description"
     }
-
-
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,12 @@
 package org.example
 
 import com.google.gson.Gson
+import com.google.gson.JsonParser
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
-import java.util.Scanner
-import kotlin.jvm.java
+import java.util.*
 
 fun main() {
     val reading = Scanner(System.`in`)
@@ -26,7 +26,39 @@ fun main() {
     val json = response.body()
 
     val gson = Gson()
-    val myInfoGames = gson.fromJson(json, InfoGame::class.java)
-    val myGames = Games(myInfoGames.info.title, myInfoGames.info.thumb)
-    println(myGames)
+    val jsonElement = JsonParser.parseString(json)
+    var myInfoGames: InfoGame? = null
+    if (jsonElement.isJsonObject){
+        myInfoGames = gson.fromJson(json, InfoGame::class.java)
+    } else if (jsonElement.isJsonArray){
+        println("Invalid id. Try again")
+        return
+    }
+
+    var myGame: Games? = null
+    val result = runCatching {
+        if (myInfoGames != null){
+            myGame = Games(myInfoGames.info.title, myInfoGames.info.thumb)
+        }
+    }
+
+    result.onFailure {
+        println("Error searching for the game. Please try again.")
+    }
+
+    result.onSuccess {
+        if (myInfoGames != null){
+            println("Do you want to enter a personalized description? S/N")
+            val option = reading.nextLine()
+
+            if (option.equals("S", true)){
+                println("Enter custom description for the game:")
+                val description = reading.nextLine()
+                myGame?.description = description
+            } else {
+                myGame?.description = myGame.title
+            }
+            println(myGame)
+        }
+    }
 }
