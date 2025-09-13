12e723f0b7506bb76fb9f365955adcfade7126bb
Nathalia Pinheiro <Thenaiapinn@gmail.com>
2025-09-13 17:58:49 -0300

    including dynamic search in the game API

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,19 +5,25 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.util.Scanner
 import kotlin.jvm.java
 
 fun main() {
+    val reading = Scanner(System.`in`)
+    println("Enter a game code to search: ")
+    val search = reading.nextLine()
+
+    val address = "https://www.cheapshark.com/api/1.0/games?id=$search"
+
    val client: HttpClient = HttpClient.newHttpClient()
    val request = HttpRequest.newBuilder()
-        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .uri(URI.create(address))
         .build()
 
     val response = client
         .send(request, HttpResponse.BodyHandlers.ofString())
 
     val json = response.body()
-    println(json)
 
     val gson = Gson()
     val myInfoGames = gson.fromJson(json, InfoGame::class.java)
