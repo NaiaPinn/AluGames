4883684ae9b617ae1c22ae8689919b0c2b32b361
Nathalia Pinheiro <Thenaiapinn@gmail.com>
2025-09-29 20:00:06 -0300

    Create GamerExtension.kt

---
+++ src/main/kotlin/br/com/naiapinn/alugames/utility/GamerExtension.kt
@@ -0,0 +1,8 @@
+package br.com.naiapinn.alugames.utility
+
+import br.com.naiapinn.alugames.model.Gamer
+import br.com.naiapinn.alugames.model.InfoGamerJson
+
+fun InfoGamerJson.createGamer(): Gamer{
+    return Gamer(this.name, this.email, this.birthDate, this.userName)
+}
