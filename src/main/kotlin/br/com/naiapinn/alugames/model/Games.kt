bf905eb9c8506428c8275f0971d5cc8856d185b6
Nathalia Pinheiro <Thenaiapinn@gmail.com>
2025-09-29 20:38:51 -0300

    Update Games.kt

--- src/main/kotlin/br/com/naiapinn/alugames/model/Games.kt
+++ src/main/kotlin/br/com/naiapinn/alugames/model/Games.kt
@@ -3,10 +3,18 @@ package br.com.naiapinn.alugames.model
 class Games(val title: String?, val cover: String?) {
 
     var description = ""
+    var price = 0.0
+
+    constructor(title: String?, cover: String?, description: String, price: Double ): this(title, cover) {
+        this.price = price
+        this.description = description
+    }
+
     override fun toString(): String {
         return "My Games: \n" +
                 "Title: $title \n" +
                 "Cover: $cover \n" +
-                "Description: $description"
+                "Description: $description" +
+                "Price: $price"
     }
 }
