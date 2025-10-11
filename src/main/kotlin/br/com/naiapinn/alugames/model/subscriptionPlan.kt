9933014a306f6b6cd071ab31c999bf1fb40f7ccd
Nathalia Pinheiro <Thenaiapinn@gmail.com>
2025-10-11 07:36:30 -0300

    Update subscriptionPlan.kt

--- src/main/kotlin/br/com/naiapinn/alugames/model/subscriptionPlan.kt
+++ src/main/kotlin/br/com/naiapinn/alugames/model/subscriptionPlan.kt
@@ -1,7 +1,17 @@
 package br.com.naiapinn.alugames.model
 
-class subscriptionPlan(
+class SubscriptionPlan(
     type: String,
     val monthlyFee: Double,
     val includedGames: Int): Plan(type) {
+
+    override fun obtainPrice(rent: Rent): Double {
+       val totalGamesPerMonth = rent.gamer.gameOfTheMonth(rent.period.startDate.monthValue).size+1
+       return if (totalGamesPerMonth <= includedGames) {
+           0.0
+       } else {
+           super.obtainPrice(rent)
+       }
+
+    }
 }
