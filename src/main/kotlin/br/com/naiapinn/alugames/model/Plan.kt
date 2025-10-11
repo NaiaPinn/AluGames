782b77d5c0976083700c44d9875bdefca5670cbe
Nathalia Pinheiro <Thenaiapinn@gmail.com>
2025-10-11 07:36:47 -0300

    Open fun

--- src/main/kotlin/br/com/naiapinn/alugames/model/Plan.kt
+++ src/main/kotlin/br/com/naiapinn/alugames/model/Plan.kt
@@ -2,7 +2,7 @@ package br.com.naiapinn.alugames.model
 
 open class Plan(val type: String) {
 
-    fun obtainPrice(rent: Rent): Double {
+    open fun obtainPrice(rent: Rent): Double {
         return rent.games.price * rent.period.inDays
 
     }
