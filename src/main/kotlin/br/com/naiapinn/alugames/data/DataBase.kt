ad59d262ebe2aae79c77be168e86566c2186423c
Nathalia Pinheiro <Thenaiapinn@gmail.com>
2025-10-14 07:08:30 -0300

    Update DataBase.kt

--- src/main/kotlin/br/com/naiapinn/alugames/data/DataBase.kt
+++ src/main/kotlin/br/com/naiapinn/alugames/data/DataBase.kt
@@ -1,42 +1,13 @@
 package br.com.naiapinn.alugames.data
 
-import br.com.naiapinn.alugames.model.Games
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException
+import javax.persistence.EntityManager
+import javax.persistence.EntityManagerFactory
+import javax.persistence.Persistence
 
 object DataBase {
 
-    fun getConnection(): Connection? {
-        return try {
-            DriverManager.getConnection("jdbc:mysql://localhost:3306/alugames", "your user", "your password")
-        } catch (e: SQLException) {
-            e.printStackTrace()
-            null
-        }
-    }
-
-    fun getGames(): List<Games> {
-        val listGames = mutableListOf<Games>()
-        val connection = getConnection()
-        if (connection != null){
-            try {
-                val statement = connection.createStatement()
-                val result = statement.executeQuery("SELECT * FROM GAMES")
-                while (result.next()) {
-                    val id = result.getInt("id")
-                    val title = result.getString("title")
-                    val cover =  result.getString("cover")
-                    val description =  result.getString("description")
-                    val price = result.getDouble("price")
-                    val game = Games(title, cover, description, price, id)
-                    listGames.add(game)
-                }
-                statement.close()
-            } finally {
-                connection.close()
-            }
-        }
-        return listGames
+    fun getEntityManager(): EntityManager {
+        val factory: EntityManagerFactory = Persistence.createEntityManagerFactory("alugames")
+        return factory.createEntityManager()
     }
 }
